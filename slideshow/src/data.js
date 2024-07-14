89fcda24aa89c03fa8303251f339d0f05f5be617
NiloufarSheyd <155656698+NiloufarSheid@users.noreply.github.com>
2024-07-14 20:19:11 +0330

    start slideshow project

---
+++ slideshow/src/data.js
@@ -0,0 +1,24 @@
+const person=[
+   { id:1,
+    name:"نیلوفر",
+    job:"طراح وب",
+    Image:"profileone.png",
+    text:"اذیلذزنتیذنتزلیلزتنیذزنتیلزنیذزتیذ"
+
+    },
+    {
+        id:2,
+        name:"احسان",
+        job:" برنامه نویس",
+        Image:"profiletwo.png",
+        text:"اذیلذزنتیذنتزلیلزتنیذزنتیلزنیذزتیذ" 
+    },
+    {
+        id:3,
+        name:"تینا",
+        job:"  معلم",
+        Image:"profilethree.png",
+        text:"اذیلذزنتیذنتزلیلزتنیذزنتیلزنیذزتیذ"  
+    }
+]
+export default person
