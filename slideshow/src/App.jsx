50ba70f37b6cfaaa6bab45695ea869dd279e952a
NiloufarSheyd <155656698+NiloufarSheid@users.noreply.github.com>
2024-07-17 21:25:13 +0330

    add css

--- slideshow/src/App.jsx
+++ slideshow/src/App.jsx
@@ -1,4 +1,5 @@
 import Review from "./Review"
+
 function App() {
   
 
--- slideshow/src/Review.jsx
+++ slideshow/src/Review.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { FaChevronRight,FaChevronLeft} from 'react-icons/fa';
 
 const Review = () => {
   return (
-    <div>
-        hi
+    <div className='review'>
+        <div className="review-img">
+            <img src='' />
+        </div>
+        <h4 className='author'>نیلوفر شید</h4>
+        <p className='job'></p>
+        <p className='info'></p>
+        <div className="review-button">
+            <button><FaChevronRight/></button>
+            <button><FaChevronLeft/></button>
+        </div>
+        <button className='random-btn'></button>
     </div>
   );
 }
